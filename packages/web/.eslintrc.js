9a266410131eaa77d7cd39cd05a3ff8b8fc5ab9a
Hiroki Imahama <hiroki.imahama@on-sight.ne.jp>
2020-12-31 08:13:23 +0900

    [UPDATE]質問リストの表示まで完成

--- packages/web/.eslintrc.js
+++ packages/web/.eslintrc.js
@@ -1,84 +1,52 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-  },
+  parserOptions: { ecmaVersion: 2021, sourceType: 'module', ecmaFeatures: { jsx: true } },
+  settings: { react: { version: 'detect' } },
+  env: { es2021: true, browser: true, jest: true, node: true },
+  plugins: ['import', 'simple-import-sort', 'react-hooks'],
   extends: [
-    'airbnb-typescript',
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:react/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
     'prettier/react',
-    'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    project: './tsconfig.json',
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ['@typescript-eslint', 'import', 'jsx-a11y', 'prettier', 'react', 'react-hooks'],
   rules: {
-    // eslint
-    'global-require': 'off',
-    'consistent-return': 'off',
-    'newline-before-return': 'error',
-    'no-console': 'off',
-    'no-restricted-syntax': 'off',
-    'no-unused-expressions': ['error', { allowShortCircuit: true }],
-    'no-underscore-dangle': 'off',
-    'no-nested-ternary': 'off',
-    // @typescript-eslint
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/prefer-interface': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    //prettier
-    'prettier/prettier': [
-      'warn',
-      {},
-      {
-        usePrettierrc: true,
-      },
-    ],
-    // import
-    'import/extensions': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    // jsx-a11y
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-noninteractive-element-interactions': 'off',
-    'jsx-a11y/img-redundant-alt': 'off',
-    // react
-    'react/jsx-filename-extension': [
+    'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
+    'no-restricted-syntax': [
       'error',
-      {
-        extensions: ['.jsx', '.tsx'],
-      },
+      { selector: 'TSEnumDeclaration', message: "Don't declare enums" },
     ],
-    'react/prop-types': 'off',
-    'react/jsx-one-expression-per-line': 'warn',
-    'react/jsx-wrap-multilines': 'off',
-    'react/no-array-index-key': 'off',
-    'react/display-name': 'off',
-    'react/button-has-type': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    // react-hooks
+    'prefer-arrow-callback': 'error',
+    'func-style': ['error', 'expression'],
+    'arrow-body-style': ['error', 'always'],
+    'no-restricted-imports': ['error', { paths: [{ name: 'react', importNames: ['default'] }] }],
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 'error',
+    'react/destructuring-assignment': ['error', 'never'],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-  },
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': ['off'],
+    'import/newline-after-import': 'error',
+    'import/no-default-export': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
       },
-    },
-  ],
-};
+    ],
+  },
+}
---
+++ packages/web/stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      { ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'] },
+    ],
+    'comment-empty-line-before': null,
+  },
+}
